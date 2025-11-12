f69c529fddd05e0bc294f03635cace926c8ed191
Wilker Martins <dev.wilker@gmail.com>
2025-11-12 17:46:53 -0300

    removendo chat imprestaveldfdfghj

--- src/components/Automation.tsx
+++ src/components/Automation.tsx
@@ -37,9 +37,9 @@ const Automation = () => {
               </h3>
               <p className="text-lg text-gray-700 leading-relaxed">
                 {/* Texto Provisório (podes alterar) */}
-                {/* CORREÇÃO AQUI: Trocámos "low-code" por 'low-code'
+                {/* CORREÇÃO AQUI: 'low-code' está agora entre chavetas {} 
                 */}
-                O N8N é uma ferramenta 'low-code' de automação de fluxos de
+                O N8N é uma ferramenta {'low-code'} de automação de fluxos de
                 trabalho. Permite-me conectar diferentes aplicações e serviços
                 (APIs, bancos de dados, e-mails) para criar processos
                 automáticos de forma visual e eficiente.
@@ -81,10 +81,10 @@ const Automation = () => {
               </h4>
               <p className="text-gray-600">
                 {/* Texto Provisório (podes alterar) */}
-                {/* CORREÇÃO AQUI: Trocámos "conversem" por 'conversem'
+                {/* CORREÇÃO AQUI: 'conversem' está agora entre chavetas {} 
                 */}
                 Conecto APIs de serviços distintos (Google, CRMs, E-mail) para
-                que 'conversem' entre si.
+                que {'conversem'} entre si.
               </p>
             </div>
             {/* Card 3 */}
